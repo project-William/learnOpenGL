1ae63ad2d4ba03e178ff0633d726ae6e148760ea
project-William <williamms669@gmail.com>
2019-02-10 20:42:53 +0800

    update

--- RayTracing/Main.cpp
+++ RayTracing/Main.cpp
@@ -1,13 +1,25 @@
 #include "Vec3.h"
-
+#include <fstream>
 int main() {
-	Vec3<float> vector1(1.0f,1.0f,1.0f);
-	Vec3<float> vector2(1.0f, 2.0f, 2.0f);
-	Vec3<float> vector3;
-	
-	vector2.normalize();
+	int nx = 200;
+	int ny = 100;
 
-	vector2.display();
-	system("PAUSE");
+	std::ofstream outfile;
+	outfile.open("image.ppm");
+
+	outfile << "P3\n" << nx << " " << ny << "\n255\n";
+
+	for (int i = ny - 1; i >= 0; i--) {
+		for (int j = 0; j < nx; j++) {
+			Vec3<float> vector(float(j) / nx, float(i) / ny, 0.2f);
+			int ir = int(255.99*(vector.get_r()));
+			int ig = int(255.99*(vector.get_g()));
+			int ib = int(255.99*(vector.get_b()));
+			outfile << ir << " " << ig << " " << ib << "\n";
+		}
+	}
+
+	outfile.close();
+
 	return 0;
 }
