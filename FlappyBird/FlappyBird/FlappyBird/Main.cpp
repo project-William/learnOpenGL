3968fd1e981edcaad149e5189252d8f2d729a9ab
project-William <williamms669@gmail.com>
2019-05-24 21:58:17 +0800

    add math: 2D-vector(Vec2)

--- FlappyBird/FlappyBird/FlappyBird/Main.cpp
+++ FlappyBird/FlappyBird/FlappyBird/Main.cpp
@@ -1,12 +1,13 @@
 #include "src/Application.h"
-
+using namespace flappy::math;
 
 int main() {
-	Application* app = new Application();
-	
-	app->run();
+	Vec2 vector( 0.0f, 1.0f);
+	Vec2 vector1(1.0f, 0.0f);
+	float f = getDegrees(vector,vector1);
+	std::cout << f << std::endl;
+	std::cout << vector.get_x() << "," << vector.get_y() << std::endl;
 
-	delete app;
 
 	return 0;
 }
--- FlappyBird/FlappyBird/FlappyBird/src/EntryPoint.h
+++ FlappyBird/FlappyBird/FlappyBird/src/EntryPoint.h
@@ -1,9 +1,10 @@
 #pragma once
 
+//math 
+#include "math/Vec2.h"
 //window
 #include "graphics/Window.h"
 //input
 #include "utils/Input.h"
 //shader
 #include "graphics/Shader.h"
-
